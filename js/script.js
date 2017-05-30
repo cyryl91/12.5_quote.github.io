216518da8a83a3332878039310e20f3298ccbf9c
cyryl91 <91.cyryl@gmail.com>
2017-05-30 16:16:07 +0200

    script is not run

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-$(function(){  //ładuje jQuery
+$(function(){
 
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
